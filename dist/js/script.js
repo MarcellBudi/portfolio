14abddc445611e4641d5cec4409c7e68d6c8c114
Marcell Budi <marcellbudiputra@gmail.com>
2022-08-23 11:32:13 +0800

    upload portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,32 @@
+// Navbar Fixed
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
