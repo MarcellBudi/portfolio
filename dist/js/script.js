260074b63936751773032fb061be78b753651cb5
Marcell Budi <marcellbudiputra@gmail.com>
2022-08-30 12:11:40 +0800

    fix hero section start

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,12 +20,29 @@ window.onscroll = function(){
 // Hamburger
 
 const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const navMenu = document.querySelector('#navbar');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// let navbarlinks = select('#navbar .scrollto', true) 
+// const navbarlinksActive = () => {
+//   let position = window.scrollY + 200
+//   navbarlinks.forEach(navbarlink => {
+//     if (!navbarlink.hash) return
+//     let section = select(navbarlink.hash)
+//     if (!section) return
+//     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+//       navbarlink.classList.add('active')
+//     } else {
+//       navbarlink.classList.remove('active')
+//     }
+//   })
+// }
+// window.addEventListener('load', navbarlinksActive);
+// onscroll(document, navbarlinksActive);
 
 // const portfolioLightbox = GLightbox({
 //     selector: '.portfolio-lightbox'
