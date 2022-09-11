b07fc16be083a30d1733030b89da7532e7acf640
Marcell Budi <marcellbudiputra@gmail.com>
2022-09-11 21:21:30 +0800

    fix navbar display

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,23 +42,46 @@ gsap.to('.lead', {duration: 3, delay: 1.5, text: 'Web Developer & Web Design'});
 gsap.from('.navbar', {duration: 1.5, y: '-100%', opacity: 0, ease: 'bounce' });
 gsap.from('.display', {duration: 1, x: -50, opacity: 0, delay: 0.5, ease: 'back' });
 
-// let navbarlinks = select('#navbar .scrollto', true) 
-// const navbarlinksActive = () => {
-//   let position = window.scrollY + 200
-//   navbarlinks.forEach(navbarlink => {
-//     if (!navbarlink.hash) return
-//     let section = select(navbarlink.hash)
-//     if (!section) return
-//     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-//       navbarlink.classList.add('active')
-//     } else {
-//       navbarlink.classList.remove('active')
-//     }
-//   })
-// }
-// window.addEventListener('load', navbarlinksActive);
-// onscroll(document, navbarlinksActive);
+(function() {
+  "use strict";
 
-// const portfolioLightbox = GLightbox({
-//     selector: '.portfolio-lightbox'
-// });
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true) 
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+})()
