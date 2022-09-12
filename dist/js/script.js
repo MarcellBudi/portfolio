c964173893d5c9c81f8cd3cb2c20fadad56a31a6
Marcell Budi <marcellbudiputra@gmail.com>
2022-09-12 21:02:03 +0800

    fix navbar display

--- dist/js/script.js
+++ dist/js/script.js
@@ -85,41 +85,73 @@ gsap.from('.display', {duration: 1, x: -50, opacity: 0, delay: 0.5, ease: 'back'
   onscroll(document, navbarlinksActive)
 
   /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  //  let selectHeader = select('#header')
+  //  if (selectHeader) {
+  //    const headerScrolled = () => {
+  //      if (window.scrollY > 100) {
+  //        selectHeader.classList.add('header-scrolled')
+  //      } else {
+  //        selectHeader.classList.remove('header-scrolled')
+  //      }
+  //    }
+  //    window.addEventListener('load', headerScrolled)
+  //    onscroll(document, headerScrolled)
+  //  }
+ 
+   /**
+    * Back to top button
+    */
+  //  let backtotop = select('.back-to-top')
+  //  if (backtotop) {
+  //    const toggleBacktotop = () => {
+  //      if (window.scrollY > 100) {
+  //        backtotop.classList.add('active')
+  //      } else {
+  //        backtotop.classList.remove('active')
+  //      }
+  //    }
+  //    window.addEventListener('load', toggleBacktotop)
+  //    onscroll(document, toggleBacktotop)
+  //  }
+
+  /**
    * Mobile nav toggle
    */
-   on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  //  on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile')
+  //   this.classList.toggle('bi-list')
+  //   this.classList.toggle('bi-x')
+  // })
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  // on('click', '.navbar .dropdown > a', function(e) {
+  //   if (select('#navbar').classList.contains('navbar-mobile')) {
+  //     e.preventDefault()
+  //     this.nextElementSibling.classList.toggle('dropdown-active')
+  //   }
+  // }, true)
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  // on('click', '.scrollto', function(e) {
+  //   if (select(this.hash)) {
+  //     e.preventDefault()
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
+  //     let navbar = select('#navbar')
+  //     if (navbar.classList.contains('navbar-mobile')) {
+  //       navbar.classList.remove('navbar-mobile')
+  //       let navbarToggle = select('.mobile-nav-toggle')
+  //       navbarToggle.classList.toggle('bi-list')
+  //       navbarToggle.classList.toggle('bi-x')
+  //     }
+  //     scrollto(this.hash)
+  //   }
+  // }, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
